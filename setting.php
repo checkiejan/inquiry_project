2c149b9096d8741d01ec2004d6b5df6564295f51
hung <hungtrangia2003@gmail.com>
2023-06-27 12:15:06 +1000

    1

--- setting.php
+++ setting.php
@@ -1,8 +1,8 @@
 <?php
-  $host= "feenix-mariadb.swin.edu.au";
-  $user= "s103509199";
-  $pwd= "hungdeptraivl";
-  $sql_db= "s103509199_db";
+  $host= "";
+  $user= "";
+  $pwd= "";
+  $sql_db= "";
   function sanitise_input($data) {
     $data = trim($data);
     $data = stripslashes($data);
